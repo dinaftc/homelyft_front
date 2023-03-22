e537c336ce8806a3ba1a61e2f3451d94c436bf19
dinaftc <88668587+dinaftc@users.noreply.github.com>
2023-03-23 00:17:55 +0100

    Update Signup.js

--- yac1/frontend/src/Signup.js
+++ yac1/frontend/src/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import {  IconButton, InputAdornment, TextField } from "@mui/material";
 import { Link } from 'react-router-dom';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import TagIcon from '@mui/icons-material/Tag';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import { FcGoogle } from 'react-icons/fc';
@@ -50,32 +50,45 @@ function SignUp() {
  
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <form className="bg-white border rounded-lg p-7 shadow-black hover:">
-        <h2 className="text-4xl font-bold font-sans p-8 text-primary">
-          Sign Up for HomeLyft
-        </h2>
-        <p>Create an account and start enjoying your shopping</p>
-         
-        
-        {!showWelcome && <div className="my-2 ">
-          <TextField
-          onChange={handleUserChange}
-            label="Full name"
-            value={fullname}
-            required
-            className="hover:outline-primary w-full font-pop "
-            size="small"
-            type={"username"}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <PersonIcon/>
-                </InputAdornment>
-              ),
-            }}
-          />
+      <form className="bg-white rounded-lg py-4 px-12" onSubmit={handleSubmit}>
+      <div className="my-6">
+          <h1 className="text-4xl font-bold font-pop  text-center text-primary ">
+            Sign up
+          </h1>
+          <div className="my-3 flex flex-col text-center px-2">
+            <p className="text-gray-500 font-pop">
+            Create an account and start enjoying your shopping
+            </p>
+            </div>
+            </div>
+        {!showWelcome && <div className="my-3" >
+        <TextField
+  onChange={handleUserChange}
+  label="Full name"
+  value={fullname}
+  required
+  className="hover:outline-primary w-full font-pop "
+  size="small"
+  type="username"
+  InputProps={{
+    endAdornment: (
+      <InputAdornment position="end">
+        <PersonIcon />
+      </InputAdornment>
+    ),
+  }}
+  style={{
+    backgroundColor: "#FFFFFB",
+    border: "2px ",
+    borderRadius: "10px",
+    
+  }}
+/>
+
+
+
         </div>}
-        {!showWelcome && <div className="my-2 ">
+        {!showWelcome && <div className="my-3 ">
           <TextField
           onChange={handleEmailChange}
             label="Email"
@@ -91,9 +104,20 @@ function SignUp() {
                 </InputAdornment>
               ),
             }}
+            InputLabelProps={{
+              classes: {
+                root: 'font-pop',
+              },
+            }}
+            style={{
+              backgroundColor: "#FFFFFB",
+              border: "2px ",
+              borderRadius: "10px",
+              
+            }}
           />
         </div>}
-        {!showWelcome && <div className="my-2 ">
+        {!showWelcome && <div className="my-3 ">
         <TextField
   onChange={handlePhoneChange}
   label="Phone Number"
@@ -106,14 +130,25 @@ function SignUp() {
     endAdornment: (
       <InputAdornment position="end">
         
-        <LocalPhoneIcon/>
+        <TagIcon/>
       </InputAdornment>
     ),
+  }}
+  InputLabelProps={{
+    classes: {
+      root: 'font-pop',
+    },
+  }}
+   style={{
+    backgroundColor: "#FFFFFB",
+    border: "2px ",
+    borderRadius: "10px",
+    
   }}
 />
         </div>}
       
-       {!showWelcome && <div className="my-2">
+       {!showWelcome && <div className="my-3">
           <TextField
           onChange={handlePasswordChange}
             label="Password"
@@ -131,6 +166,17 @@ function SignUp() {
                   </IconButton>
                 </InputAdornment>
               ),
+            }} 
+            InputLabelProps={{
+              classes: {
+                root: 'font-pop',
+              },
+            }}
+            style={{
+              backgroundColor: "#FFFFFB",
+              border: "2px ",
+              borderRadius: "10px",
+              
             }}
           />
         </div>}
@@ -140,34 +186,36 @@ function SignUp() {
         
            
 
-        <button
-          className="btn btn-active rounded-lg text-white w-full bg-primary normal-case hover:bg-hoveredLog outline-none"
-          onClick={handleSubmit}
-        >
-          Sign Up
-        </button>
-        <div className="flex flex-row justify-center">
+        <div className="my-7">
+          <button className="btn btn-active rounded-lg text-white w-full bg-primary normal-case hover:bg-hoveredLog outline-none "
+          type='submit'>
+            Sign up
+          </button>
+          <div className="flex flex-row justify-center">
             <div className="my-1 text-center ">
-              <p className="text-sm text-gray-500 font-pop">
-                already have an account ?
+              <p className="text-sm inline-block text-gray-500 font-pop">
+                already have an account ?{"  "}
               </p>
-            </div>
-            <div className="my-1 mx-1">
-              <Link to='/' className="text-sm text-primary font-pop hover:text-hoveredLog">
-                Login
+            
+            
+              <Link to='/' 
+              className="inline-block text-sm text-primary font-pop hover:text-hoveredLog">
+                Login{"   "}
               </Link>
-            </div>
-            </div>
-            <div className="container w-full flex justify-center">
+           
+           </div>
+          </div>
+          
+<div className="container w-full flex justify-center">
           <div className="flex flex-col w-full border-opacity-200">
           <div className="divider">OR</div>
            
-          
-          <button className="btn btn-outline text-black w-full normal-case rounded-lg  gap-2 my-2 outline-gray-500  hover:bg-hovered   ">
+          </div></div>
+          <button className="btn btn-outline  w-full normal-case rounded-lg  gap-2 my-2 outline-gray-500  border-gray-500 "
+          >
             <FcGoogle/>
             Sign up with Google
           </button>
-        </div>
         </div>
       </form>
       {showWelcome && (

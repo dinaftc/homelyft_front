be95369e813244ed3da76416d9f8c6012dc5e8b1
guezzen <t.guezzen@esi-sba.dz>
2023-04-03 22:34:58 +0100

    without changing icons

--- yac1/frontend/tailwind.config.js
+++ yac1/frontend/tailwind.config.js
@@ -2,18 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {colors: {
-      transparent: 'transparent',
-      primary: '#1B5789',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-    },},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        primary: "#1B5789",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
+        hovered: "#E4F8F3",
+        actif: "#25A5AA",
+      },
+      fontFamily: {
+        pop: ["Poppins"],
+        popBold: ["Poppins"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };

12a3bdcde3ce76292a103bb19b897ce59a28d9a4
dinaftc <d.fettache@esi-sba.dz>
2023-05-12 20:05:49 +0100

    sprint 4

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,18 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import Navbar from './Navbar';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import Navbar from "./Navbar";
 import axios from "axios";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function Home({ isAuthenticated,user }) {
+function Home({ isAuthenticated, user }) {
   const [products, setProducts] = useState([]);
   const [showPopup, setShowPopup] = useState(false);
   const [selectedProductId, setSelectedProductId] = useState(null);
   const [quantity, setSelectedQuantity] = useState(1);
+  const [Categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/homeLift/categories/")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
- 
-  
   useEffect(() => {
     fetch("http://127.0.0.1:8000/homeLift/products/")
       .then((response) => response.json())
@@ -29,22 +36,21 @@ function Home({ isAuthenticated,user }) {
     setShowPopup(true);
   };
 
- 
-
   const handleAddToCartSubmit = async () => {
     if (selectedProductId !== null && quantity > 0) {
       if (!isAuthenticated) {
-        alert('Please sign up or login to buy');
+        alert("Please sign up or login to buy");
       } else {
-      try {  const response = await axios.post(
-          `http://127.0.0.1:8000/home/${user.id}/${selectedProductId}/addtocart/`,
-          { Quantity: quantity },
-          { headers: { 'Content-Type': 'application/json' } }
-        );
-          
+        try {
+          const response = await axios.post(
+            `http://127.0.0.1:8000/home/${user.id}/${selectedProductId}/addtocart/`,
+            { Quantity: quantity },
+            { headers: { "Content-Type": "application/json" } }
+          );
+
           console.log(response.data);
-          if  (response.status >= 200 && response.status < 300 ){
-            toast.success('Added to cart', {
+          if (response.status >= 200 && response.status < 300) {
+            toast.success("Added to cart", {
               position: "top-center",
               autoClose: 5000,
               hideProgressBar: false,
@@ -53,11 +59,9 @@ function Home({ isAuthenticated,user }) {
               draggable: true,
               progress: undefined,
               theme: "light",
-              });
-          }
-          else
-           {
-            toast.error('Product already exist', {
+            });
+          } else {
+            toast.error("Product already exist", {
               position: "top-center",
               autoClose: 5000,
               hideProgressBar: false,
@@ -66,13 +70,11 @@ function Home({ isAuthenticated,user }) {
               draggable: true,
               progress: undefined,
               theme: "light",
-              });
+            });
           }
-        } 
-         catch (error) {
+        } catch (error) {
           console.error(error);
         }
-        
       }
     }
     setShowPopup(false);
@@ -80,130 +82,150 @@ function Home({ isAuthenticated,user }) {
     setSelectedQuantity(1);
   };
 
- 
   return (
-    
-  
     <div>
-    {(user || !isAuthenticated) && (
-      <>
-        {(isAuthenticated && user.role === 3) || !isAuthenticated ? (
-          <div>
-          <Navbar></Navbar>
-          <div className="card-container bg-white" style={{ display: "flex" }}>
-            {products.map((product, index) => (
-              <div className="card card-compact w-96 bg-base-100 shadow-xl m-5">
-                <figure className="h-2/3">
-                  <img src={product.image} alt={product.name} />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{product.name}</h2>
-                  <p>{product.description}</p>
-                  <p>Price: {product.price} DA</p>
-                  <div className="card-actions justify-end">
-                    <button
-                      className="btn bg-primary border-none outline-none"
-                      onClick={() => handleAddToCartClick(product.id)}
-                    >
-                      Add to cart
-                    </button>
-                  </div>
-                </div>
-              </div>
-              
-            ))}
-            {showPopup && (
-              <div className="popup">
-                <div className="popup-inner">
-                  <h3>Enter Quantity</h3>
-                  <input
-                    type="number"
-                    value={quantity}
-                    onChange={(e) => setSelectedQuantity(e.target.value)}
-                  />
-                  <button onClick={handleAddToCartSubmit}>Add to Cart</button>
-                  <button onClick={() => setShowPopup(false)}>Cancel</button>
-                </div>
-                
-              </div>
-            )}
-            <style jsx>{`
-              .card-container {
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: flex-start;
-              }
-  
-              .card {
-                flex: 1 0 20%;
-                height: 400px;
-              }
-              .card-body {
-                padding: 0.5rem;
-  
-                display: flex;
-                justify-content: space-between;
-              }
-  
-              .card-title {
-                margin-top: 0.2rem;
-                font-size: 1rem;
-              }
-              .popup {
-                position: fixed;
-                width: 100%;
-                height: 100%;
-                top: 0;
-                left: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                background-color: rgba(0, 0, 0, 0.7);
-              }
-              .popup-inner {
-                width: 50%;
-                padding: 2rem;
-                background-color: white;
-                border-radius: 5px;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-              }
-              .popup-inner h3 {
-                margin-top: 0;
-              }
-              .popup-inner input {
-                margin-bottom: 1rem;
-              }
-              .popup-inner button {
-                margin: 0.5rem;
-                padding: 0.5rem 1rem;
-                border: none;
-                border-radius: 5px;
-                background-color: #3f51b5;
-                color: white;
-                font-size: 1rem;
-                cursor: pointer;
-                transition: all 0.2s ease-in-out;
-              }
-              .popup-inner button:hover {
-                background-color: #1a237e;
-              }
-            `}</style>
-          </div></div>
-        ) : (
-          <p>admin</p>
-        )}
-      </>
-    )}
-    <ToastContainer/>
+      {(user || !isAuthenticated) && (
+        <>
+          {(isAuthenticated && user.role === 3) || !isAuthenticated ? (
+            <div>
+              <Navbar></Navbar>
+              <div
+                className="card-container bg-white"
+                style={{ display: "flex" }}
+              >
+              {Categories.map((category) => (
+  <div key={category.id} className="relative inline-block">
+    <select className="select w-36 max-w-xs bg-transparent outline-none border-none focus:bg-offwhite">
+      <option disabled selected>
+        {category.name}
+      </option>
+      {category.subCategories.map((subcategory) => (
+        <option key={subcategory.id} className="bg-transparent outline-none border-none hover:bg-primary focus:bg-offwhite">
+          {subcategory.name}
+        </option>
+      ))}
+    </select>
+   
   </div>
-)}  
-  const mapStateToProps = (state) => ({
-    isAuthenticated: state.auth.isAuthenticated,
-    user : state.auth.user,
-  });
-  
-  export default connect(mapStateToProps, { })(Home);
+))}
+<hr />
+                {products.map((product, index) => (
+                  <div className="card card-compact w-96 bg-base-100 shadow-xl m-5">
+                    <figure className="h-2/3">
+                      <img src={product.image} alt={product.name} />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{product.name}</h2>
+                      <p>{product.description}</p>
+                      <p>Price: {product.price} DA</p>
+                      <div className="card-actions justify-end">
+                        <button
+                          className="btn bg-primary border-none outline-none"
+                          onClick={() => handleAddToCartClick(product.id)}
+                        >
+                          Add to cart
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                {showPopup && (
+                  <div className="popup">
+                    <div className="popup-inner">
+                      <h3>Enter Quantity</h3>
+                      <input
+                        type="number"
+                        value={quantity}
+                        onChange={(e) => setSelectedQuantity(e.target.value)}
+                      />
+                      <button onClick={handleAddToCartSubmit}>
+                        Add to Cart
+                      </button>
+                      <button onClick={() => setShowPopup(false)}>
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                )}
+                <style jsx>{`
+                  .card-container {
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: flex-start;
+                  }
+
+                  .card {
+                    flex: 1 0 20%;
+                    height: 400px;
+                  }
+                  .card-body {
+                    padding: 0.5rem;
+
+                    display: flex;
+                    justify-content: space-between;
+                  }
+
+                  .card-title {
+                    margin-top: 0.2rem;
+                    font-size: 1rem;
+                  }
+                  .popup {
+                    position: fixed;
+                    width: 100%;
+                    height: 100%;
+                    top: 0;
+                    left: 0;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    background-color: rgba(0, 0, 0, 0.7);
+                  }
+                  .popup-inner {
+                    width: 50%;
+                    padding: 2rem;
+                    background-color: white;
+                    border-radius: 5px;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+                  }
+                  .popup-inner h3 {
+                    margin-top: 0;
+                  }
+                  .popup-inner input {
+                    margin-bottom: 1rem;
+                  }
+                  .popup-inner button {
+                    margin: 0.5rem;
+                    padding: 0.5rem 1rem;
+                    border: none;
+                    border-radius: 5px;
+                    background-color: #3f51b5;
+                    color: white;
+                    font-size: 1rem;
+                    cursor: pointer;
+                    transition: all 0.2s ease-in-out;
+                  }
+                  .popup-inner button:hover {
+                    background-color: #1a237e;
+                  }
+                `}</style>
+              </div>
+            </div>
+          ) : (
+            <p>admin</p>
+          )}
+        </>
+      )}
+      <ToastContainer />
+    </div>
+  );
+}
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  user: state.auth.user,
+});
+
+export default connect(mapStateToProps, {})(Home);

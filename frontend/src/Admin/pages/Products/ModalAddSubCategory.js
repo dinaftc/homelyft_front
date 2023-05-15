7a910cb39e2c92f990559af130c37516b084b3d4
dinaftc <88668587+dinaftc@users.noreply.github.com>
2023-05-15 18:26:44 +0100

    Update ModalAddSubCategory.js

--- frontend/src/Admin/pages/Products/ModalAddSubCategory.js
+++ frontend/src/Admin/pages/Products/ModalAddSubCategory.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useState } from "react";
+
 const ModalAddSubCategory = ({
   showModalSub,
   setShowModalSub,
@@ -6,13 +8,19 @@ const ModalAddSubCategory = ({
   Categories,
   setCategories,
 }) => {
- 
-  const addSubCategory = (categoryId, subCategoryName) => {
+  const [subCategoryName, setSubCategoryName] = useState("");
+
+  const addSubCategory = () => {
+    const subcategoryObj = { name: subCategoryName };
+    const subcategoryJson = JSON.stringify(subcategoryObj);
     axios
       .post(
         `http://127.0.0.1:8000/homeLift/categories/${categoryId}/subcategory-create/`,
+        subcategoryJson,
         {
-          name: subCategoryName,
+          headers: {
+            "content-type": "application/json",
+          },
         }
       )
       .then((response) => {
@@ -28,13 +36,10 @@ const ModalAddSubCategory = ({
           }
         });
         setCategories(updatedCategories);
-        setShowModalSub(!showModalSub);
+        setShowModalSub(false);
       })
       .catch((error) => console.error(error));
-      setShowModalSub(!showModalSub);
   };
-  //const subCategoryInput = document.getElementById("sub-category-input");
-  //const newSubCategory = subCategoryInput.value.trim();
 
   return (
     <div className="flex justify-center items-center h-screen w-screen fixed top-0 left-0 bg-opacity-50 bg-gray-900 z-50">
@@ -50,10 +55,12 @@ const ModalAddSubCategory = ({
             id="sub-category-input"
             class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 border border-gray-400 bg-transparent rounded-lg border-1  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer"
             placeholder=" "
+            value={subCategoryName}
+            onChange={(e) => setSubCategoryName(e.target.value)}
           />
           <label
             for="sub-category-input"
-            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
           >
             Sub-Category
           </label>
@@ -61,7 +68,7 @@ const ModalAddSubCategory = ({
         <div className="flex flex-row  mt-5 justify-end gap-3">
           <div>
             <button
-              onClick={() => setShowModalSub(!showModalSub)}
+              onClick={() => setShowModalSub(false)}
               className="font-pop btn btn-outline normal-case bg-gray-300 text-black hover:bg-gray-200 rounded-full border-transparent hover:border-transparent hover:text-black"
             >
               Cancel
@@ -69,8 +76,7 @@ const ModalAddSubCategory = ({
           </div>
           <div>
             <button
-             
-              onChange={addSubCategory(categoryId,"hello")}
+              onClick={addSubCategory}
               className=" font-pop w-20 btn normal-case text-white rounded-full bg-primary  border-primary hover:bg-primary hover:border-primary"
             >
               Add{" "}
@@ -83,3 +89,5 @@ const ModalAddSubCategory = ({
 };
 
 export default ModalAddSubCategory;
+
+

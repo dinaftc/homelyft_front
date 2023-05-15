52172d11b1d22e221bd27e5b57089d3448b266df
guezzen <t.guezzen@esi-sba.dz>
2023-05-15 19:18:47 +0100

    sayi hadi w mnzidch xd

--- frontend/src/Home/ProductInfo.js
+++ frontend/src/Home/ProductInfo.js
@@ -30,8 +30,14 @@ const ProductInfo = () => {
 
   return (
     <div className="">
-      <div className="flex p-20  ">
-        <div className="w-2/3 grid grid-cols-2"></div>
+      <div className="flex px-10 py-5  ">
+        <div className="w-2/3 grid grid-cols-2 border border-gray-500 p-5 rounded-lg">
+          <img src={products[1].image} />
+          
+          <img src={products[1].image} />
+          <img src={products[1].image} />
+          <img src={products[1].image} />
+        </div>
         <div className=" ml-10 w-1/3 space-y-2">
           <p className="text-lg font-bold font-pop">Product name</p>
           <div className="flex flex-row">
@@ -76,9 +82,11 @@ const ProductInfo = () => {
           </div>
         </div>
       </div>
-      <div className="mt-10">
+      <div className="mt-5">
         <div>
-          <p className=" px-20 font-pop font-bold text-xl">Recommended for you </p>
+          <p className=" px-20 font-pop font-bold text-xl">
+            Recommended for you{" "}
+          </p>
         </div>
         <div>
           <div className="">
@@ -133,7 +141,7 @@ const ProductInfo = () => {
                         </span>{" "}
                         {product.price} DA
                       </p>
-                      
+
                       <div className="flex justify-between ">
                         <div className="flex justify-start mt-2 space-x-3 cursor-pointer">
                           <Icon className="">
@@ -179,7 +187,7 @@ const ProductInfo = () => {
               <style jsx>{`
                 .card-container {
                   display: flex;
-                  width:full;
+                  width: full;
                   justify-content: flex-start;
                 }
 
@@ -246,22 +254,46 @@ const ProductInfo = () => {
         </div>
       </div>
       <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
-  <div className="grid grid-flow-col gap-4">
-    <a className="link link-hover">About us</a> 
-    <a className="link link-hover">Contact</a> 
-
-    
-  </div> 
-  <div>
-    <div className="grid grid-flow-col gap-4">
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-    </div>
-  </div> 
-  <div>
-    <p>Copyright © 2023 - All right reserved by<span className="text-primary font-pop font-bold text-lg ml-1"> HomeLift</span> </p>
-  </div>
-</footer>
+        <div className="grid grid-flow-col gap-4">
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+        </div>
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div>
+          <p>
+            Copyright © 2023 - All right reserved by
+            <span className="text-primary font-pop font-bold text-lg ml-1">
+              {" "}
+              HomeLift
+            </span>{" "}
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
